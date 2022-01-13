8eabaf0b7ddca637f861a358e8249e0831347dbb
ubdbra001 <dan.t.brady21@gmail.com>
2022-01-13 15:17:45 +0000

    added stimuli_areas()

--- R/data_plotting_functions.R
+++ R/data_plotting_functions.R
@@ -19,3 +19,21 @@ calculate_points <- function(x_points, y_points, group = NA){
   return(points_list)
 }
 
+
+stimuli_areas <- function() {
+  
+  # Produces the default location for the stimuli presented on Screen
+  L_x_vals = c(0.16, 0.32)
+  R_x_vals = c(0.68, 0.84)
+  y_vals = c(0.36, 0.64)
+  
+  L_stim <- calculate_points(L_x_vals, y_vals)
+  R_stim <- calculate_points(R_x_vals, y_vals)
+  
+  gap <- tibble(x = NA, y = NA)
+  
+  stims <- bind_rows(L_stim, gap, R_stim)
+  
+  return(stims)
+}
+
