c959acc41af7eea6930a25030ba868bc60c4a850
ubdbra001 <dan.t.brady21@gmail.com>
2022-01-13 15:52:43 +0000

    Added comments and default look length

--- R/user_defined_constants.R
+++ R/user_defined_constants.R
@@ -4,6 +4,7 @@ Left_y = c(0, 1)
 Right_x = c(0.6, 1)
 Right_y = c(0, 1)
 
+# Slider bar properties
 lower_limit = 0
 upper_limit = 1
 
@@ -13,7 +14,10 @@ step_size = 0.01
 # Constants for plotting
 hjust = 1.1
 vjust = 1
+y_lims = c(0, 1)
 
-y_lims = c(0,1)
-
+# Sample rate for calculating sample time
 sample_rate = 120
+
+# Default for look length
+look_length_default = 12
