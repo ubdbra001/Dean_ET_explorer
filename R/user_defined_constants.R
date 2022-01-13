c50c8450b60e35302fdb04b2f8ce7e0dca257e82
ubdbra001 <dan.t.brady21@gmail.com>
2022-01-13 15:48:44 +0000

    Move constants to their own file

---
+++ R/user_defined_constants.R
@@ -0,0 +1,19 @@
+# default AOI properties
+Left_x = c(0, 0.4)
+Left_y = c(0, 1)
+Right_x = c(0.6, 1)
+Right_y = c(0, 1)
+
+lower_limit = 0
+upper_limit = 1
+
+step_size = 0.01
+
+
+# Constants for plotting
+hjust = 1.1
+vjust = 1
+
+y_lims = c(0,1)
+
+sample_rate = 120
