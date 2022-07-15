a3a9b0c544b54f72a524e3e016137c0f176b7355
Dan Brady <dan.t.brady21@gmail.com>
2022-07-15 17:09:34 +0100

    Remove code that was turned into a dedicated function

--- app.R
+++ app.R
@@ -140,34 +140,11 @@ server <- function(input, output) {
           ET_cat <- ET_categorised()
         }
         
-        # Calculate when the first look in a specific AOI occurs
-        # (first look defined as first incidence of a run of samples categorised
-        # as within an AOI for the user input specified amount, default is 12)
-        ET_fl <- group_by(ET_cat, part_ID) %>%
-          summarise(.groups = "keep",
-            first_L = find_first_look(AOI_L, input$samples_for_look),
-            first_R = find_first_look(AOI_R, input$samples_for_look))
+        # 
+        ET_cat <- add_first_look(ET_cat, input$samples_for_look)
         
-        # Categorise which of these two first looks came first and thus which is
-        # the true first look
-        ET_fl <- mutate(ET_fl, 
-            FL = case_when(is.na(first_L) & is.na(first_R) ~ NA_character_,
-                           ((first_L < first_R) | is.na(first_R)) ~ "L",
-                           ((first_R < first_L) | is.na(first_L)) ~ "R"))
-        
-        # Add the first look for each participant and trial back to the samples
-        # for those trials
-        ET_cat <- left_join(ET_cat, ET_fl, by = "part_ID")
-        
-        # Determine whether an individual sample matches the first look for that
-        # participant/trial
-        ET_cat <- mutate(ET_cat,
-                         at_first_look = case_when(FL == "L" ~ AOI_L,
-                                                   FL == "R" ~ AOI_R,
-                                                   is.na(FL) ~ NA),
-                         at_first_look = case_when(
-                             (AOI_L | AOI_R) == T ~ at_first_look))
-        
+        # Summarise across participants to show how many participants were
+        # looking per sample
         ET_cat <- group_by(ET_cat, sample_ID, Group)
         
         ET_cat <- summarise(ET_cat, .groups = "keep", 
